b062b62f1a11bcea7780cdb5afac19f15cb3a7cd
jay <dj.min43@gmail.com>
2021-12-05 08:20:29 +0900

    add maps and slices

--- tutorial-edge/composite-types/main.go
+++ tutorial-edge/composite-types/main.go
@@ -2,7 +2,29 @@ package main
 
 import "fmt"
 
-func main () {
+func array () {
 	days := [...]string{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
 	fmt.Println(days)
+}
+
+func slices () {
+	// Slices are made up of three things, a pointer, a length, and a capacity. 
+	days := [...]string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+	weekdays := days[0:5]
+	fmt.Println(weekdays)
+}
+
+func maps () {
+	youtubeSubscribers := map[string]int{
+		"TutorialEdge":     2240,
+		"MKBHD":            6580350,
+		"Fun Fun Function": 171220,
+	}
+	fmt.Println(youtubeSubscribers["MKBHD"])	
+}
+
+func main () {
+	array()
+	slices()
+	maps()
 }
