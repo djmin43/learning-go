24c02ef3c07d4b0f1646affbfd869e308f18a1b9
jay <dj.min43@gmail.com>
2021-12-05 15:01:16 +0900

    methods

---
+++ tutorial-edge/methods/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	Name string
+}
+
+func (e *Employee) UpdateName(newName string) {
+	e.Name = newName
+}
+
+func (e *Employee) PrintName() {
+	fmt.Println(e.Name)
+}
+
+func main() {
+	var employee Employee
+	employee.Name = "Elliot"
+	employee.UpdateName("Austin")
+	employee.PrintName()
+}
