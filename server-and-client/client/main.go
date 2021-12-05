1b6a75aa6921e8c3f6253a612a11aa0be4be7f75
jay <dj.min43@gmail.com>
2021-12-05 14:25:29 +0900

    remove error code for simplicity

--- server-and-client/client/main.go
+++ server-and-client/client/main.go
@@ -7,14 +7,9 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:18888")
-	if err != nil {
-		panic(err)
-	}
+	resp, _ := http.Get("http://localhost:18888")
+	log.Println(resp)
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+	body, _ := ioutil.ReadAll(resp.Body)
 	log.Println(string(body))
 }
