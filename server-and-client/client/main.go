b1bd7b0cfd4922491b2aa45d3784f29630f608e2
james <james.min@freedgrouptech.com>
2021-12-15 07:19:23 +0900

    feat: write a multipart post client

--- server-and-client/client/main.go
+++ server-and-client/client/main.go
@@ -1,9 +1,36 @@
 package main
 
-import "mime/multipart"
+import (
+	"bytes"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
 
 func main() {
-	var buffer bytes.buffer
+	var buffer bytes.Buffer
 	writer := multipart.NewWriter(&buffer)
-	
+	writer.WriteField("name", "Michael Jackson")
+
+	fileWriter, err := writer.CreateFormFile("thumbnail", "photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	readFile, err := os.Open("photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	defer readFile.Close()
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	resp, err := http.Post("http://loalhost:18888", writer.FormDataContentType(), &buffer)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
 }
