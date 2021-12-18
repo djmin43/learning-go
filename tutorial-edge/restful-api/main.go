5415ba2a71c6f7bf126303a05551e8e358c3da1e
jay <dj.min43@gmail.com>
2021-12-18 10:45:26 +0900

    feat: add println

--- tutorial-edge/restful-api/main.go
+++ tutorial-edge/restful-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -28,9 +29,13 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article{
 		{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
 		{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
