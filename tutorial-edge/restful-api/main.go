e4b922467c7db0b17328fbfb00404c272f3e2b77
jay <dj.min43@gmail.com>
2021-12-19 09:14:39 +0900

    feat: add api for single item

--- tutorial-edge/restful-api/main.go
+++ tutorial-edge/restful-api/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 type Article struct {
+	Id	string `json:"id"`
 	Title string `json:"Title"`
 	Desc string `json:"desc"`
 	Content string `json:"content"`
@@ -27,18 +28,26 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Articles)
 }
 
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Fprintf(w, "Key: " + key)
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/suus", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
 	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article{
-		{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{Id: "2" ,Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 }
 	handleRequests()
 }
