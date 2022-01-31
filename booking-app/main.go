33490bd5b2bca3d012f4ec32e1c96f7bb139839a
jay <dj.min43@gmail.com>
2022-01-31 10:25:32 +0900

    feat: uset ypes

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,9 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conferecne booking application")
+	var conferenceName = "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var conferenceName = "Go Conference"
-	fmt.Println(conferenceName)
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	
 }
