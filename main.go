f0dd773b9b921ff1b349736f6660282b4ddf0629
jay <dj.min43@gmail.com>
2022-04-02 23:08:28 +0900

    add quote package
    this adds quote package to the main.go file.
    - this package will be gone anytime soon

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/djmin43/learning-go/mascot"
+	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(mascot.BestMascot())
+	fmt.Println(quote.Go())
 }
