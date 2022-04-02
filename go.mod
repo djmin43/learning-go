5437d390198f8f4c9f67ca84f071af9010628183
jay <dj.min43@gmail.com>
2022-04-02 22:52:54 +0900

    add main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/djmin43/learning-go
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/djmin43/learning-go/mascot"
+)
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println(mascot.BestMascot())
+}
