561ccc12573bbf756cd9269cce802ca9bcbd362d
jay <dj.min43@gmail.com>
2022-04-03 17:07:11 +0900

    cleans up practice code

--- main.go
+++ main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/djmin43/learning-go/mascot"
+	"rsc.io/quote"
 )
 
 func main() {
-	// fmt.Println("hello world")
-	// fmt.Println(mascot.BestMascot())
-	// fmt.Println(quote.Go())
-	// fmt.Println("Greetings and \n\"Salutations\"")
-	isStringLarger := "a" > "b"
-	fmt.Println(isStringLarger)
-	fmt.Println(typeConversion())
-}
-
-func typeConversion () float64{
-	var x int = 10
-	var y float64 = 30.2
-	var z float64 = float64(x) + y
-	return z
+	fmt.Println("hello world")
+	fmt.Println(mascot.BestMascot())
+	fmt.Println(quote.Go())
 }
