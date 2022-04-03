fd4d0d31b574cbc00697ae1c61b4ebd0e533336c
jay <dj.min43@gmail.com>
2022-04-03 11:58:32 +0900

    add a type conversion function
    Go does not allow automatic type promotion
    - that being said, when i want to calculate each numbers, I should manually convert type

--- main.go
+++ main.go
@@ -11,4 +11,12 @@ func main() {
 	// fmt.Println("Greetings and \n\"Salutations\"")
 	isStringLarger := "a" > "b"
 	fmt.Println(isStringLarger)
+	fmt.Println(typeConversion())
+}
+
+func typeConversion () float64{
+	var x int = 10
+	var y float64 = 30.2
+	var z float64 = float64(x) + y
+	return z
 }
