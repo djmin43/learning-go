059e7ec057ef64b92274910015996caadcf0a62c
james <james.min@freedgrouptech.com>
2022-04-05 07:20:33 +0900

    declare arrays in go
    there are different ways to declare arrays in go:
    - var x[2]int as an array with two empty items
    - var x = [2]int{10, 20}
    - var a = [...]int{1,2,3}
    - var b[2][3], this is nested array

--- main.go
+++ main.go
@@ -11,4 +11,15 @@ func main() {
 	fmt.Println("hello world")
 	fmt.Println(mascot.BestMascot())
 	fmt.Println(quote.Go())
+	declareArray()
+}
+
+func declareArray() {
+	var x [3]int
+	var y = [3]int{10, 20, 30}
+	var a = [...]int{1, 2, 3}
+	var b [2][3]int
+	fmt.Println(a, b, x, y)
+	// arrays in Go has different types per sizes. Go also can't use type conversion to convert arrays of different sizes to identical types.
+	// It is almost impossible to use array in Go unless you know the size of the array for sure.
 }
