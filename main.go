e849ab49a2f704464af588b0c95724f36bb11a11
james <james.min@freedgrouptech.com>
2022-04-08 07:35:28 +0900

    notes about slices

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Println(mascot.BestMascot())
 	fmt.Println(quote.Go())
 	declareArray()
+	declareSlices()
 }
 
 func declareArray() {
@@ -22,4 +23,25 @@ func declareArray() {
 	fmt.Println(a, b, x, y)
 	// arrays in Go has different types per sizes. Go also can't use type conversion to convert arrays of different sizes to identical types.
 	// It is almost impossible to use array in Go unless you know the size of the array for sure.
+}
+
+func declareSlices() {
+	var a = []int{10, 20, 30, 40}
+	fmt.Println(a)
+	// var b [][]int
+	var c []int
+	fmt.Println(c)
+	// nil is an identifier that represents the lack of a value for some types
+	// nil has no type
+	// slices cannot be compared
+	fmt.Println(c == nil)
+	var d []int
+	d = append(d, 10)
+	fmt.Println(d)
+
+	e := make([]int, 5)
+	fmt.Println(e)
+	f := make([]int, 5, 10)
+	fmt.Println(f)
+
 }
