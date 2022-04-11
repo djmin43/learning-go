229774cd5c914b70c2505d0554e1a4a090b7dd68
james <james.min@freedgrouptech.com>
2022-04-11 09:28:30 +0900

    clean up array

--- main.go
+++ main.go
@@ -11,37 +11,12 @@ func main() {
 	fmt.Println("hello world")
 	fmt.Println(mascot.BestMascot())
 	fmt.Println(quote.Go())
-	declareArray()
 	declareSlices()
 }
 
-func declareArray() {
-	var x [3]int
-	var y = [3]int{10, 20, 30}
-	var a = [...]int{1, 2, 3}
-	var b [2][3]int
-	fmt.Println(a, b, x, y)
-	// arrays in Go has different types per sizes. Go also can't use type conversion to convert arrays of different sizes to identical types.
-	// It is almost impossible to use array in Go unless you know the size of the array for sure.
-}
-
 func declareSlices() {
-	var a = []int{10, 20, 30, 40}
-	fmt.Println(a)
-	// var b [][]int
-	var c []int
-	fmt.Println(c)
-	// nil is an identifier that represents the lack of a value for some types
-	// nil has no type
-	// slices cannot be compared
-	fmt.Println(c == nil)
-	var d []int
-	d = append(d, 10)
-	fmt.Println(d)
 
-	e := make([]int, 5)
-	fmt.Println(e)
-	f := make([]int, 5, 10)
+	f := make([]int, 0, 10)
 	fmt.Println(f)
 
 	// when we create a slice, the primary goal is to minimize the number of times the slice needs to grow.
