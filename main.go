124a67cd855e212be3a14983c075fd7c84f18923
james <james.min@freedgrouptech.com>
2022-04-13 09:22:47 +0900

    slicing slices

--- main.go
+++ main.go
@@ -11,15 +11,8 @@ func main() {
 	fmt.Println("hello world")
 	fmt.Println(mascot.BestMascot())
 	fmt.Println(quote.Go())
-	declareSlices()
 }
 
-func declareSlices() {
+func slicingSlices() {
 
-	f := make([]int, 0, 10)
-	fmt.Println(f)
-
-	// when we create a slice, the primary goal is to minimize the number of times the slice needs to grow.
-	// slice literal is a good choice if values are not going to change
-	// if not sure, use make.
 }
