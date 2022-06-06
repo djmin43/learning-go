94fe205c1971da54c973b86ae03cc88eb50dc38b
james <james.min@freedgrouptech.com>
2022-06-06 10:06:43 +0900

    for-range loop

--- main.go
+++ main.go
@@ -3,19 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 100; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuzz")
-			continue
-		}
-		if i%3 == 0 {
-			fmt.Println("Fizz")
-			continue
-		}
-		if i%5 == 0 {
-			fmt.Println("Buzz")
-			continue
-		}
-		fmt.Println(i)
+
+	evenVals := []int{2, 4, 6, 8, 10, 12}
+	for i, v := range evenVals {
+		fmt.Println(i, v)
 	}
 }
